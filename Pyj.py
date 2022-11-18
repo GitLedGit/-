0c1e899d48fe1053029b8a98786a33188f00b816
GitLedGit <118552036+GitLedGit@users.noreply.github.com>
2022-11-18 21:26:51 +0200

    Add files via upload
    Pyj it is a programming language in python. There are two files here. Pyj this is a file for testing, PyjWork but for work. But you can rename the file PyjWork to import. Leave a comment.

---
+++ Pyj.py
@@ -0,0 +1,76 @@
+def ran():
+    n = 0 | 1 | 2 | 3 | 4 | 5
+    a = 6 | 7 | 8 | 9 | 10 | 11
+    b = 11 | 12 | 13 | 14| 15 | 16
+    BI = n + a + b
+    
+def pr(p):
+    print(p)
+    
+def hel():
+    print("Hello World")
+    
+def Pro():
+    print("Hello Programmer")
+    
+def ser():
+    spi = input()
+    
+def peV(v):
+    pr("|" * v)
+    
+def peni(n):
+    pr(n)
+    
+def pek(k):
+    if k == "s":
+        pr("()")
+    if k == "k2":
+        pr("  ..  ")
+        pr(" .  . ")
+        pr(".    .")
+        pr(".    .")
+        pr(" .  . ")
+        pr("  ..  ")
+    if k == "s2":
+        pr("{}")
+        
+def button(Name, but, Command):
+    pr("/" + Name)
+    pr("{" + but)
+    CMN = Command
+    Code = input(str("Code button:"))
+    if Code == but:
+        CMN
+        
+def anime(Object, DI):
+    if Object  == "Coob":
+        if DI == "UP":
+            for x in range(1, 10):
+                pr("")
+            pr(" ===== ")
+            pr("|     |")
+            pr("|     |")
+            pr("|     |")
+            pr(" ===== ")
+            for x in range(1, 10):
+                pr("")
+            pr(" ===== ")
+            pr("|     |")
+            pr("|     |")
+            pr("|     |")
+            pr(" ===== ")
+            for x in range(1, 4):
+                pr("")
+            
+    
+        
+pr("PI")
+hel()
+Pro()
+anime("Coob", "UP")
+button("Start", "123", anime("Coob", "UP"))
+pek("s")
+peni("?")
+peV(12)
+ser()
